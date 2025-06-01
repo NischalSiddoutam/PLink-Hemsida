4d43c68bcf97125841e90291830804e660a4cf16
NischalSiddoutam <nischa060706@gmail.com>
2025-06-01 02:28:57 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
+import Stats from "./components/Stats";
+import Footer from "./components/Footer";
+import Cta from "./components/Cta";
+
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+      <Stats />
+      <Cta />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
